a10b454d530b3ef2289cef8a004f0291ed489882
BatteredBunny <ayes2022@protonmail.com>
2022-04-07 19:08:45 +0300

    files

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
 use gloo::{console::log, events::EventListener};
-use rand::{thread_rng, Rng};
-use wasm_bindgen::{prelude::*, JsCast};
+use wasm_bindgen::{JsCast, prelude::*};
 use wasm_bindgen_futures::{JsFuture, spawn_local};
-use web_sys::{Blob, Request, RequestInit, Response, Url};
+use web_sys::{Blob, Request, RequestInit, RequestRedirect, Response, Url, Window};
+
+use crate::code_generation::CodeGenerator;
+
+mod code_generation;
 
 #[wasm_bindgen(start)]
-pub async fn main() {   
+pub async fn main() {
     let window = web_sys::window().expect("no global `window` exists");
     let document = window.document().expect("should have a document on window");
 
@@ -17,12 +20,16 @@ pub async fn main() {
         document.get_element_by_id("images").unwrap().set_inner_html("");
     });
 
-    let on_click_generate = EventListener::new(&generate_btn, "click", move |_|  {
+    let on_click_generate = EventListener::new(&generate_btn, "click", move |_| {
+        log!("Starting image search");
         spawn_local(generate(1));
+        log!("Finished image search");
     });
 
     let on_click_generate_ten = EventListener::new(&generate_ten_btn, "click", move |_| {
+        log!("Starting image search");
         spawn_local(generate(10));
+        log!("Finished image search");
     });
 
     on_click_clear.forget();
@@ -42,7 +49,6 @@ async fn generate(amount: i32) {
     let link_length_input = document.get_element_by_id("link_length").unwrap();
     let link_length: i32 = link_length_input.get_attribute("value").unwrap().parse().unwrap();
 
-    log!("Starting image search");
     spinner.remove_attribute("hidden").unwrap();
 
     clear_btn.set_attribute("disabled", "").unwrap();
@@ -50,24 +56,28 @@ async fn generate(amount: i32) {
     generate_ten_btn.set_attribute("disabled", "").unwrap();
     link_length_input.set_attribute("disabled", "").unwrap();
 
-    let mut counter = 0;
-    while amount > counter {
-        log!("Generating...");
-        if let Ok((blob, source)) = fetch_image(link_length).await {
-            log!("Found image");
+    let mut generator = CodeGenerator::new(link_length as usize);
+
+    let mut opts = RequestInit::new();
+    opts.method("GET").redirect(RequestRedirect::Manual);
+
+    let mut found: u16 = 0;
+    while found < amount as u16 {
+        if let Ok(res) = fetch(&opts, &window, &mut generator).await {
+            log!("Found image", &res.origin_url);
 
             let a = document.create_element("a").unwrap();
-            a.set_attribute("href", &source).unwrap();
+            a.set_attribute("href", &res.origin_url).unwrap();
 
             let img = document.create_element("img").unwrap();
             img.set_class_name("generated_image");
-            img.set_attribute("src", &blob).unwrap();
+            img.set_attribute("src", &res.blob).unwrap();
 
             a.append_child(&img).unwrap();
 
             images.prepend_with_node_1(&a).unwrap();
-            counter += 1;
-        }   
+            found += 1;
+        }
     }
 
     spinner.set_attribute("hidden", "").unwrap();
@@ -78,38 +88,31 @@ async fn generate(amount: i32) {
     link_length_input.remove_attribute("disabled").unwrap();
 }
 
-async fn fetch_image(link_length: i32) -> Result<(String, String), JsValue> {
-    let mut opts = RequestInit::new();
-    opts.method("GET");
+struct FetchResult {
+    blob: String,
+    origin_url: String,
+}
 
-    let window = web_sys::window().unwrap();
-
-    let request = Request::new_with_str_and_init(&generate_code(link_length, "https://i.imgur.com/"), &opts)?;
+async fn fetch(request_options: &RequestInit, window: &Window, generator: &mut CodeGenerator) -> Result<FetchResult, JsValue> {
+    let request = Request::new_with_str_and_init(generator.generate(), request_options)?;
     let response_raw = JsFuture::from(window.fetch_with_request(&request)).await?;
     let response: Response = response_raw.clone().dyn_into()?;
 
-    let blob: Blob = JsFuture::from(response.blob().unwrap()).await?.dyn_into()?;
+    match response.status() {
+        429 => {
+            Err(JsValue::from("Rate limited by imgur"))
+        },
+        302 => {
+            Err(JsValue::from("Invalid image"))
+        }
+        200 => {
+            let blob: Blob = JsFuture::from(response.blob().unwrap()).await?.dyn_into()?;
 
-    let url = response.url();
-    log!(&url);
-
-    if &url == "https://i.imgur.com/removed.png" {
-        Err(JsValue::from_str("Image removed"))
-    } else {
-        Ok(
-            (Url::create_object_url_with_blob(&blob).unwrap(), url)
-        )
+            Ok(FetchResult {
+                blob: Url::create_object_url_with_blob(&blob).unwrap(),
+                origin_url: response.url(),
+            })
+        }
+        _ => Err(JsValue::from("Unexpected")),
     }
-}
-
-fn generate_code(length: i32, site: &str) -> String {
-    let mut rng = thread_rng();
-
-    const CHAR_POOL: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-    let code: String = (0..length)
-        .map(|_| CHAR_POOL[rng.gen_range(0..CHAR_POOL.len())] as char)
-        .collect();
-
-    format!("{}{}.jpg", site, code)
 }
