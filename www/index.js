8cbe83ebd36abbd5b067e256d341c21faba7af83
BatteredBunny <ayes2022@protonmail.com>
2022-04-07 16:10:14 +0300

    files

--- www/index.js
+++ www/index.js
@@ -1,12 +1,1 @@
-import * as wasm from "wasm-imgur-brute";
-
-document.getElementById("button").addEventListener("click", () => {
-    wasm.generate(document.getElementById("link-length").value);
-});
-
-document.getElementById("auto").addEventListener("click", () => {
-    let link_length = document.getElementById("link-length").value;
-    for (let i = 1; i < 10; i++) {
-        wasm.generate(link_length);
-    }
-});
+import * as wasm from "wasm-imgur-bruteforce";
